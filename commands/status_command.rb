9912788a9fd53b2a12d826063ebb3dd09e773b7f
CriminalTalent <rntjdrud@naver.com>
2026-06-13 23:57:54 +0900

    Update status_command.rb

--- commands/status_command.rb
+++ commands/status_command.rb
@@ -10,21 +10,23 @@ class StatusCommand
   def execute(content:, account:, status_id:)
     return "먼저 `[등록/캐릭터명]`을 해주세요." unless @sheet.registered?(account)
 
-    name = @sheet.user_name(account)
+    name   = @sheet.user_name(account)
     credit = @sheet.get_credit(account)
-    stats = @sheet.stats(account)
+    pts    = @sheet.get_stat_points(account)
+    st     = @sheet.stats(account)
 
     <<~TEXT.strip
       #{name} 학생의 현재 기록입니다.
 
       크레딧: #{credit}
+      스탯 포인트 잔여: #{pts}pt
 
-      건강: #{stats["건강"]}
-      마법능력: #{stats["마법능력"]}
-      내구도: #{stats["내구도"]}
-      민첩: #{stats["민첩"]}
-      기술: #{stats["기술"]}
-      행운: #{stats["행운"]}
+      건강: #{st["건강"]}
+      마법능력: #{st["마법능력"]}
+      인내: #{st["인내"]}
+      속도: #{st["속도"]}
+      기술: #{st["기술"]}
+      행운: #{st["행운"]}
     TEXT
   end
 end
